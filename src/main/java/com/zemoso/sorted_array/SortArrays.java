7a737a8b949d6b5d45286fc6801bd7919308f132
chinni-zemoso <chinni.srungavarapu@zemosolabs.com>
2020-10-21 16:41:16 +0530

    Sorting of 2 arrays

---
+++ src/main/java/com/zemoso/sorted_array/SortArrays.java
@@ -0,0 +1,38 @@
+package com.zemoso.sorted_array;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SortArrays {
+
+    private static final Logger logger = LoggerFactory.getLogger(SortArrays.class);
+
+    public static void main(String[] args) {
+        int[] firstArray = {1, 4, 7, 19, 23};
+        int[] secondArray = {2, 5, 7, 16, 51, 67};
+        int[] resultantArray = getSortedArray(firstArray, secondArray);
+        for (int value : resultantArray)
+            logger.info(value + " ");
+    }
+
+    private static int[] getSortedArray(int[] a, int[] b) {
+        int lenOfFirstArray = a.length;
+        int lengthOfSecondArray = b.length;
+        int[] resultantArray = new int[lenOfFirstArray + lengthOfSecondArray];
+        int firstArrayIndex = 0, secondArrayIndex = 0, resultantIndex = 0;
+
+        while (firstArrayIndex < lenOfFirstArray && secondArrayIndex < lengthOfSecondArray) {
+            if (a[firstArrayIndex] < b[secondArrayIndex])
+                resultantArray[resultantIndex++] = a[firstArrayIndex++];
+            else
+                resultantArray[resultantIndex++] = b[secondArrayIndex++];
+        }
+        while (firstArrayIndex < lenOfFirstArray)
+            resultantArray[resultantIndex++] = a[firstArrayIndex++];
+        while (secondArrayIndex < lengthOfSecondArray)
+            resultantArray[resultantIndex++] = b[secondArrayIndex++];
+
+        return resultantArray;
+    }
+}
+
