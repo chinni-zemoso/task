bd800d86678446afffd24d5cb65406945e1c14c8
chinni-zemoso <chinni.srungavarapu@zemosolabs.com>
2020-10-22 13:27:08 +0530

    Improvement After Discussion

--- src/main/java/com/zemoso/half_array/FindMinSet.java
+++ src/main/java/com/zemoso/half_array/FindMinSet.java
@@ -11,36 +11,38 @@ public class FindMinSet {
     private static final Logger logger = LoggerFactory.getLogger(FindMinSet.class);
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] array = {3, 3, 3, 3, 5, 5, 5, 2, 2, 7};
         logger.info(String.valueOf(getMinArraySize(array)));
     }
 
     private static int getMinArraySize(int[] array) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        //Frame, each integer occurance
-        for (int index = 0; index < array.length; index++) {
-            Integer currValue = map.get(array[index]);
-            if (currValue == null)
-                map.put(array[index], 1);
-            else
-                map.put(array[index], currValue + 1);
+        if (array.length == 0) {
+            throw new RuntimeException("Array is empty");
+        }
+        Map<Integer, Integer> mapOfElementAndOccurance = new HashMap<Integer, Integer>();
+        for (int element : array) {
+            Integer legacyValue = mapOfElementAndOccurance.get(element);
+            if (legacyValue == null) {
+                mapOfElementAndOccurance.put(element, 1);
+            } else {
+                mapOfElementAndOccurance.put(element, legacyValue + 1);
+            }
         }
         //Get, Only Occurances from Map
-        int[] occurances = new int[map.size()];
+        int[] occurances = new int[mapOfElementAndOccurance.size()];
         int occuranceIndex = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : mapOfElementAndOccurance.entrySet()) {
             occurances[occuranceIndex++] = entry.getValue();
         }
-        //Sort the array
         Arrays.sort(occurances);
         //Iterate Array, From backwards to find  "minimum size of the set"
-        int result = 0, size = array.length;
+        int resultantSetSize = 0, size = array.length;
         for (int index = occurances.length - 1; index >= 0; index--) {
             if (size <= (array.length / 2))
                 break;
-            result++;
+            resultantSetSize++;
             size -= occurances[index];
         }
-        return result;
+        return resultantSetSize;
     }
 }
--- src/main/java/com/zemoso/sorted_array/SortArrays.java
+++ src/main/java/com/zemoso/sorted_array/SortArrays.java
@@ -3,14 +3,12 @@ package com.zemoso.sorted_array;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-
 public class SortArrays {
 
     private static final Logger logger = LoggerFactory.getLogger(SortArrays.class);
 
     public static void main(String[] args) {
-        int[] firstArray = {1, 4, 7, 119, 23};
+        int[] firstArray = {1, 4, 7, 19, 23};
         int[] secondArray = {2, 5, 7, 16, 51, 67};
         int[] resultantArray = getSortedArray(firstArray, secondArray);
         for (int value : resultantArray)
@@ -18,22 +16,31 @@ public class SortArrays {
     }
 
     private static int[] getSortedArray(int[] a, int[] b) {
-        int lenOfFirstArray = a.length;
+        int lengthOfFirstArray = a.length;
         int lengthOfSecondArray = b.length;
-        int[] resultantArray = new int[lenOfFirstArray + lengthOfSecondArray];
-        int firstArrayIndex = 0, secondArrayIndex = 0, resultantIndex = 0;
-
-        while (firstArrayIndex < lenOfFirstArray && secondArrayIndex < lengthOfSecondArray) {
-            if (a[firstArrayIndex] < b[secondArrayIndex])
-                resultantArray[resultantIndex++] = a[firstArrayIndex++];
-            else
-                resultantArray[resultantIndex++] = b[secondArrayIndex++];
+        if (lengthOfFirstArray == 0 && lengthOfSecondArray == 0) {
+            throw new RuntimeException("Array is empty");
         }
-        while (firstArrayIndex < lenOfFirstArray)
-            resultantArray[resultantIndex++] = a[firstArrayIndex++];
-        while (secondArrayIndex < lengthOfSecondArray)
-            resultantArray[resultantIndex++] = b[secondArrayIndex++];
+        long totalLengthOfArrays = (long) lengthOfFirstArray + lengthOfSecondArray;
+        if (totalLengthOfArrays > Integer.MAX_VALUE) {
+            throw new RuntimeException("Length of Arrays Crossing the Max Size");
+        }
+        int[] resultantArray = new int[lengthOfFirstArray + lengthOfSecondArray];
+        int firstArrayIndex = 0, secondArrayIndex = 0, resultantArrayIndex = 0;
 
+        while (firstArrayIndex < lengthOfFirstArray && secondArrayIndex < lengthOfSecondArray) {
+            if (a[firstArrayIndex] < b[secondArrayIndex]) {
+                resultantArray[resultantArrayIndex++] = a[firstArrayIndex++];
+            } else {
+                resultantArray[resultantArrayIndex++] = b[secondArrayIndex++];
+            }
+        }
+        while (firstArrayIndex < lengthOfFirstArray) {
+            resultantArray[resultantArrayIndex++] = a[firstArrayIndex++];
+        }
+        while (secondArrayIndex < lengthOfSecondArray) {
+            resultantArray[resultantArrayIndex++] = b[secondArrayIndex++];
+        }
         return resultantArray;
     }
 }
